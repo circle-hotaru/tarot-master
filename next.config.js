4090334a6ecca114d84a4c8fe8c4db9e58787198
circlehotarux <circlehotarux@gmail.com>
2023-07-28 09:56:20 +0800

    chore: use chimeragpt

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  env: {
+    CHIMERA_API_KEY: process.env.CHIMERA_API_KEY,
+  },
 }
 
 module.exports = nextConfig
--- src/apis/openai.ts
+++ src/apis/openai.ts
@@ -1,10 +1,11 @@
-const proxyAPI = 'https://chat-api.incircles.xyz'
+const proxyAPI = 'https://chimeragpt.adventblocks.cc'
 
 export const requestOpenAI = async (messages: string[]) => {
   const requestOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.CHIMERA_API_KEY}`,
     },
     body: JSON.stringify({
       model: 'gpt-3.5-turbo',
@@ -14,7 +15,7 @@ export const requestOpenAI = async (messages: string[]) => {
 
   try {
     const response = await fetch(
-      `${proxyAPI}/v1/chat/completions`,
+      `${proxyAPI}/api/v1/chat/completions`,
       requestOptions
     )
     const data = await response.json()
