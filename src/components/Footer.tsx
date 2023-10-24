59e4083291425ecfbf45fc342454da496de0b20d
circlehotarux <circlehotarux@gmail.com>
2023-10-24 22:51:56 +0800

    chore: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+const Footer: React.FC = () => {
+  return (
+    <div className="text-center">
+      <p>
+        Made with ❤️ by{' '}
+        <Link className="font-bold" href="https://github.com/circle-hotaru">
+          circlehotarux
+        </Link>{' '}
+        on{' '}
+        <Link
+          className="font-bold"
+          href="https://github.com/circle-hotaru/tarot-master"
+        >
+          GitHub
+        </Link>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
